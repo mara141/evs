77cd73f32a2a201fe9e3d9d96ff650fc54a04137
Josue Grullon <josue@mctekk.com>
2016-03-25 11:40:50 -0400

    Instituciones: required field >create view

--- resources/views/institucion/forms/int.blade.php
+++ resources/views/institucion/forms/int.blade.php
@@ -1,19 +1,18 @@
 <div class="form-group">
 {!!Form::label('Institución:')!!}
-{!!Form::text('name',null,['class'=>'form-control','placeholder'=>'Ingrese el Nombre de la institución'])!!}
+{!!Form::text('name',null,['class'=>'form-control','placeholder'=>'Ingrese el Nombre de la institución','required'=>'required'])!!}
 </div>
 <div class="form-group">
 {!!Form::label('Alias:')!!}
-{!!Form::text('alias',null,['class'=>'form-control','placeholder'=>'Ingrese su Alias'])!!}
+{!!Form::text('alias',null,['class'=>'form-control','placeholder'=>'Ingrese su Alias','required'=>'required'])!!}
 </div>
 
 <div class="form-group">
 {!!Form::label('Teléfono:')!!}
-{!!Form::text('numero',null,['class'=>'form-control','placeholder'=>'Ingrese Número de teléfono'])!!}
+{!!Form::number('numero',null,['class'=>'form-control','placeholder'=>'Ingrese Número de teléfono','required'=>'required'])!!}
 </div>
 
 <div class="form-group">
 	{!!Form::label('Logo','Logo:')!!}
-	{!!Form::file('path')!!}
+	{!!Form::file('path',['required'=>'required'])!!}
 </div>
-
